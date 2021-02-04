532c6faeef05d726d9de6b1bf50b0fe9171a8f89
Francisco Ríos <panchorios_242@hotmail.com>
2021-02-04 18:46:43 -0300

    First Commit

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAbRN7_pmGA6OPxfFxo4QD73MxWmssh3OE",
+  authDomain: "product-hunt-cb630.firebaseapp.com",
+  projectId: "product-hunt-cb630",
+  storageBucket: "product-hunt-cb630.appspot.com",
+  messagingSenderId: "649895848988",
+  appId: "1:649895848988:web:da1bc3eacaff091a60c3d9",
+};
+
+export default firebaseConfig;
---
+++ pages/buscar.js
@@ -0,0 +1,49 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import DetallesProducto from '../components/layout/DetallesProducto';
+import Layout from '../components/layout/Layout';
+import useProductos from '../hooks/useProductos';
+
+const Buscar = () => {
+
+  const router = useRouter();
+  const { query: { q }} = router;
+
+  // Todos los productos
+  const { productos } = useProductos('creado');
+  const [ resultado, guardarResultado ] = useState([]);
+
+  useEffect(() => {
+      const busqueda = q.toLowerCase();
+      const filtro =  productos.filter(producto => {
+        return (
+          producto.nombre.toLowerCase().includes(busqueda) || 
+          producto.descripcion.toLowerCase().includes(busqueda)
+        )
+      });
+      guardarResultado(filtro);
+      
+  }, [ q, productos ]);
+
+
+  return (
+    <div>
+      <Layout>
+        <div className="listado-productos">
+            <div className="contenedor">
+              <ul className="bg-white">
+                  {resultado.map(producto => (
+                      <DetallesProducto
+                          key={producto.id}
+                          producto={producto}
+                      />
+                  ))}
+              </ul>
+            </div>
+        </div>
+      </Layout>
+    </div>
+  )
+}
+
+export default Buscar
